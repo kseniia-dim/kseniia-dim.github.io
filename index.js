3a4724418c1d0045abf797afec27f1c76a6ac198
Calamander <calamander@yandex.ru>
2024-04-03 23:44:22 +0300

    rework to modules and class library etc.

--- index.js
+++ index.js
@@ -1,139 +1,154 @@
-var ccode2feature = {};
-let motif = [];
-let ccode2lang = {};
-let lang2ccode = {};
-let country2ccode = {};
-let active_langs = {};
-let active_ccode = {};
-function onInputChange(e) {
-    var id = e.target.getAttribute("data-id");
-    motif[id][2].forEach(lang => {
-        if (e.target.checked == true) {
-            if (active_langs[lang]?.length) { // lang already selected
-                active_langs[lang].push(id);
-            } else { // lang isn't selected yet
-                active_langs[lang] = [ id ];
-                if (!lang2ccode[lang])
-                    console.log(motif[id][0], ") ", lang);
-                else {
-                    var ccode = lang2ccode[lang][0];
-                    if (active_ccode[ccode]?.length) { // country by code already selected (by different lang)
-                        active_ccode[ccode].push(lang);
-                    } else { // country isn't selected
-                        active_ccode[ccode] = [ lang ];
-                        country_set_state(ccode, true);
+"use strict";
+
+export default class {
+    ccode2feature = {};
+    motif = [];
+    ccode2lang = {};
+    lang2ccode = {};
+    country2ccode = {};
+    active_langs = {};
+    active_ccode = {};
+    hovered_countries = [];
+    active_countries = [];
+    map = null;
+
+    constructor(map) {
+        this.map = map;
+        this.fetch();
+    }
+    async fetch() {
+        await fetch("./motifs_to_langs.csv")
+            .then( (response) => response.text() )
+            .then( (text) => {
+                let span = document.createElement("span");
+                let input = document.createElement("input");
+                input.type = "checkbox";
+                input.checked = false;
+                let label = document.createElement("label");
+                span.appendChild(input);
+                span.appendChild(label);
+                let legend = document.getElementById("legend");
+                let lines = text.split(/\r?\n/);
+            for(let i = 0; lines[i] && i < lines.length; i++) {
+                let cols = lines[i].split(";");
+                this.motif[i] = cols.map(e => e.trim());
+                //console.log(this.motif[i][1]);
+                if (this.motif[i][1][0] == '"' && this.motif[i][1].at(-1) == '"') {
+                    this.motif[i][1] = this.motif[i][1].slice(1,-1).replaceAll('""','"');
+                }
+                this.motif[i][2] = this.motif[i][2].split(",").map(e => e.trim());
+                let _span = span.cloneNode(true);
+                _span.onmouseenter = (e) => this.hover_motif(e);
+                _span.onmouseleave = (e) => this.hover_motif(e);
+                _span.setAttribute("data-id",i);
+                _span.children[0].id = "in-"+i;
+                _span.children[0].onchange = (e) => this.onInputChange(e);
+                _span.children[0].setAttribute("data-id",i);
+                _span.children[1].htmlFor = _span.children[0].id;
+                _span.children[1].innerHTML = this.motif[i][0] + ' ' + this.motif[i][1];
+                legend.appendChild(_span);
+            }
+        });
+
+        await fetch("./countries_langs.csv")
+            .then( (response) => response.text() )
+            .then( (text) => {
+                let lines = text.split(/\r?\n/);
+                for(let i = 0; lines[i] && i < lines.length; i++) {
+                    let cols = lines[i].split(";");
+                    let ccode = cols[0].trim();
+                    let country = cols[1].trim();
+                    let langs = cols[2].split(",").map(e => e.trim());
+                    this.ccode2lang[ccode] = [country, langs];
+                    this.country2ccode[country] = ccode;
+                    langs.forEach(e => {
+                        if (this.lang2ccode[e])
+                            alert("redundant language ", e, "in 'countries_langs.csv line: ", line[i]);
+                        else
+                        this.lang2ccode[e] = [ccode,country];
+                    });
+                    
+                }
+        });
+    }
+    onInputChange(e) {
+        let id = e.target.getAttribute("data-id");
+        this.motif[id][2].forEach(lang => {
+            if (e.target.checked == true) {
+                if (this.active_langs[lang]?.length) { // lang already selected
+                    this.active_langs[lang].push(id);
+                } else { // lang isn't selected yet
+                    this.active_langs[lang] = [ id ];
+                    if (!this.lang2ccode[lang])
+                        console.log(this.motif[id][0], ") ", lang);
+                    else {
+                        let ccode = this.lang2ccode[lang][0];
+                        if (this.active_ccode[ccode]?.length) { // country by code already selected (by different lang)
+                            this.active_ccode[ccode].push(lang);
+                        } else { // country isn't selected
+                            this.active_ccode[ccode] = [ lang ];
+                            this.country_set_state(ccode, true);
+                        }
+                    }
+                }
+            } else {
+                this.active_langs[lang].splice(this.active_langs[lang].indexOf(id),1);
+                if (!this.active_langs[lang].length) {
+                    let ccode = this.lang2ccode[lang][0];
+                    this.active_ccode[ccode].splice(this.active_ccode[ccode].indexOf(lang),1);
+                    if (!this.active_ccode[ccode].length) {
+                        this.country_set_state(ccode, false);
                     }
                 }
             }
-        } else {
-            active_langs[lang].splice(active_langs[lang].indexOf(id),1);
-            if (!active_langs[lang].length) {
-                var ccode = lang2ccode[lang][0];
-                active_ccode[ccode].splice(active_ccode[ccode].indexOf(lang),1);
-                if (!active_ccode[ccode].length) {
-                    country_set_state(ccode, false);
-                }
-            }
-        }
-    
-    })
-    //var tgl_countries
-    //;
-}
-var hovered_countries = [];
-function hover_motif(e) {
-    if (e.type == "mouseleave") {
-        hovered_countries.forEach(ccode => {
-            map.setFeatureState(ccode2feature[ccode], {"hover": false});
-        });
-        hovered_countries = [];
-    } else {
-        var id = e.target.getAttribute("data-id");
-        var new_hovered_countries = [];
-        motif[id][2].forEach(lang => {
-            var ccode = lang2ccode[lang][0];
-            if (new_hovered_countries.indexOf(ccode) == -1)
-                new_hovered_countries.push(ccode);
-        });
-        new_hovered_countries.forEach(ccode => {
-            map.setFeatureState(ccode2feature[ccode], {"hover": true});
-        });
-        hovered_countries = new_hovered_countries;
+        
+        })
+        //let tgl_countries
+        //;
     }
-    //console.log(e.type, " ", id, hovered_countries);
 
-}
-function country_set_state(ccode, selected) {
-    //console.log(ccode, selected);
-    if (ccode2feature[ccode]) {
-        map.setFeatureState(ccode2feature[ccode], {"state": selected ? "selected" : "deselected"});
-    }
-    display_active_country(ccode2lang[ccode][0], selected);
-}
-var active_countries = [];
-function display_active_country(country, show) {
-    if (show) {
-        active_countries.push(country);
-        active_countries.sort();
-    } else
-        active_countries.splice(active_countries.indexOf(country),1);
-    var countries_block = document.getElementById("active_countries");
-    var countries_w_langs = active_countries.map(c => `${c} (${active_ccode[country2ccode[c]].join(", ")})`);
-    countries_block.innerText = (active_countries.length? active_countries.length+" - ":"") + countries_w_langs.join(", ");
-}
-fetch("./motifs_to_langs.csv")
-    .then( (response) => response.text() )
-    .then( function(text) {
-        var span = document.createElement("span");
-        var input = document.createElement("input");
-        input.type = "checkbox";
-        input.checked = false;
-        var label = document.createElement("label");
-        span.appendChild(input);
-        span.appendChild(label);
-        var legend = document.getElementById("legend");
-        var lines = text.split(/\r?\n/);
-    for(let i = 0; lines[i] && i < lines.length; i++) {
-            var cols = lines[i].split(";");
-            motif[i] = cols.map(e => e.trim());
-            //console.log(motif[i][1]);
-            if (motif[i][1][0] == '"' && motif[i][1].at(-1) == '"') {
-                motif[i][1] = motif[i][1].slice(1,-1).replaceAll('""','"');
-            }
-            motif[i][2] = motif[i][2].split(",").map(e => e.trim());
-            var _span = span.cloneNode(true);
-            _span.onmouseenter = hover_motif;
-            _span.onmouseleave = hover_motif;
-            _span.setAttribute("data-id",i);
-            _span.children[0].id = "in-"+i;
-            _span.children[0].onchange = onInputChange;
-            _span.children[0].setAttribute("data-id",i);
-            _span.children[1].htmlFor = _span.children[0].id;
-            _span.children[1].innerHTML = motif[i][0] + ' ' + motif[i][1];
-            legend.appendChild(_span);
+    country_set_state(ccode, selected) {
+        //console.log(ccode, selected);
+        if (this.ccode2feature[ccode]) {
+            this.map.setFeatureState(this.ccode2feature[ccode], {"state": selected ? "selected" : "deselected"});
         }
-});
-fetch("./countries_langs.csv")
-    .then( (response) => response.text() )
-    .then( function(text) {
-        var lines = text.split(/\r?\n/);
-        for(let i = 0; lines[i] && i < lines.length; i++) {
-            var cols = lines[i].split(";");
-            var ccode = cols[0].trim();
-            var country = cols[1].trim();
-            var langs = cols[2].split(",").map(e => e.trim());
-            ccode2lang[ccode] = [country, langs];
-            country2ccode[country] = ccode;
-            langs.forEach(e => {
-                if (lang2ccode[e])
-                    alert("redundant language ", e, "in 'countries_langs.csv line: ", line[i]);
-                else
-                lang2ccode[e] = [ccode,country];
+        this.display_active_country(this.ccode2lang[ccode][0], selected);
+    }
+
+    hover_motif(e) {
+        if (e.type == "mouseleave") {
+            this.hovered_countries.forEach(ccode => {
+                this.map.setFeatureState(this.ccode2feature[ccode], {"hover": false});
             });
-            
+            this.hovered_countries = [];
+        } else {
+            let id = e.target.getAttribute("data-id");
+            let new_hovered_countries = [];
+            this.motif[id][2].forEach(lang => {
+                let ccode = this.lang2ccode[lang][0];
+                if (new_hovered_countries.indexOf(ccode) == -1)
+                    new_hovered_countries.push(ccode);
+            });
+            new_hovered_countries.forEach(ccode => {
+                this.map.setFeatureState(this.ccode2feature[ccode], {"hover": true});
+            });
+            this.hovered_countries = new_hovered_countries;
         }
-});
+        //console.log(e.type, " ", id, hovered_countries);
+    }
 
-function select_country() {
+    display_active_country(country, show) {
+        if (show) {
+            this.active_countries.push(country);
+            this.active_countries.sort();
+        } else
+            this.active_countries.splice(this.active_countries.indexOf(country),1);
+        let countries_block = document.getElementById("active_countries");
+        let countries_w_langs = this.active_countries.map(c => `${c} (${this.active_ccode[this.country2ccode[c]].join(", ")})`);
+        countries_block.innerText = (this.active_countries.length? this.active_countries.length+" - ":"") + countries_w_langs.join(", ");
+    }
 
+    select_country() {
+
+    }
 }
